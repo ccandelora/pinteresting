d547e9c030ef2b8fdbcd9aebedda833a7621b5fe
chris.candelora@americastestkitchen.com <chris.candelora@americastestkitchen.com>
2014-02-18 21:06:27 -0500

    added aws support

--- Gemfile
+++ Gemfile
@@ -21,6 +21,7 @@ gem 'bootstrap-sass'
 gem "devise", "~> 3.2.2"
 gem 'pry-rails'
 gem 'paperclip', '~> 3.0'
+gem 'aws-sdk', '~> 1.20.0'
 
 group :doc do
   # bundle exec rake doc:rails generates the API under doc/api.
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,13 @@ Pinteresting::Application.configure do
   config.log_formatter = ::Logger::Formatter.new
   # this is for devise
   config.action_mailer.default_url_options = { :host => 'cc-pinteresting.herokuapp.com' }
+  # this sets paperclip to upoad images to amazon
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['AWS_BUCKET'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
 end
